643e28c33d8b253b3d570b0d358b3b44477c7610
Danendra AP <danendradhi@gmail.com>
2025-03-18 02:11:05 +0700

    Praktikum 3. Modal Ajax Hapus Data (Data User)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -276,7 +276,35 @@ class UserController extends Controller
     return redirect('/'); 
     }
     
+    // Untuk tampilkan form confirm delete user ajax
+    public function confirm_ajax(string $id){ 
+        $user = UserModel::find($id);
+
+        return view('user.confirm_ajax', ['user' => $user]);
+    }
+
     
+    // Menghapus data user Ajax
+    public function delete_ajax(Request $request, $id) 
+    {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
     
     
     
